20057f579fa6ebed7e720d306c192f1ead73147c
Jonathan Melo <jonathanmelo04@gmail.com>
2021-11-04 10:16:50 -0300

    toolbar hiding

--- app/src/main/java/com/example/motivation/SplashActivity.kt
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -12,6 +12,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar!!.hide()
 
     }
 }
