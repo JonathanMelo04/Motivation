4cc7d6dfa1b75ff6159074fba9026c3f86364d15
Jonathan Melo <jonathanmelo04@gmail.com>
2021-11-07 16:14:37 -0300

    Added actions to toolbar buttons and color adjustments on click

--- app/src/main/java/com/example/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.motivation.R
 import com.example.motivation.databinding.ActivityMainBinding
 import com.example.motivation.infra.MotivationConstants
 import com.example.motivation.infra.SecurityPreferences
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var mSecurityPreferences: SecurityPreferences
@@ -19,5 +20,31 @@ class MainActivity : AppCompatActivity() {
 
         mSecurityPreferences = SecurityPreferences(this)
         binding.textName.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+
+        binding.buttonNewPhrase.setOnClickListener {
+            handleNewPhrase()
+        }
+
+        binding.imageAll.setOnClickListener {
+            binding.imageAll.setColorFilter(resources.getColor(R.color.colorPrimaryVariant))
+            binding.imageHappy.setColorFilter(resources.getColor(R.color.white))
+            binding.imageMorning.setColorFilter(resources.getColor(R.color.white))
+        }
+
+        binding.imageHappy.setOnClickListener {
+            binding.imageHappy.setColorFilter(resources.getColor(R.color.colorPrimaryVariant))
+            binding.imageAll.setColorFilter(resources.getColor(R.color.white))
+            binding.imageMorning.setColorFilter(resources.getColor(R.color.white))
+        }
+
+        binding.imageMorning.setOnClickListener {
+            binding.imageMorning.setColorFilter(resources.getColor(R.color.colorPrimaryVariant))
+            binding.imageAll.setColorFilter(resources.getColor(R.color.white))
+            binding.imageHappy.setColorFilter(resources.getColor(R.color.white))
+        }
+    }
+
+    private fun handleNewPhrase() {
+
     }
 }
